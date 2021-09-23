4051b37ec819cc32fdffbb939857f9659c12e6ba
toledolautaro <lautarotoledo.dev@gmail.com>
2021-09-22 22:20:16 -0300

    primer commit

---
+++ TP_1/src/calculadora.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : calculadora.c
+ Author      : github -> @toledolautaro
+ Version     : v1
+ Description : Trabajo Practico N° 1
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+
+
+	return EXIT_SUCCESS;
+}
